a0ec94616138da6d1c37ee3b870a3b123fc4d456
Alexandre Luiz <alexandre.cerutti@live.com>
2023-01-08 21:34:39 -0300

    implementations

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,11 +1,22 @@
+import { useTranslation } from "react-i18next";
 import Translator from "../translator";
 
 
 
 function Nav() {
+    const { t } = useTranslation();
 
     return (
-        <Translator />
+        <nav>
+            <span>{t("app_title")}</span>
+            <Translator />
+            <ul>
+                <li><a href="#about"><span>01.</span>{t("about_me")}</a></li>
+                <li><a href="#abilities"><span>02.</span>{t("abilities")}</a></li>
+                <li><a href="#projects"><span>03.</span>{t("projects")}</a></li>
+                <li><a href="" className="resume-link">{t("resume")}</a></li>
+            </ul>
+        </nav>
     );
 
 }
