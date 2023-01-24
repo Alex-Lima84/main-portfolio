fd0da323ced096df4bd5695776a268766443f7c9
Alexandre Luiz <alexandre.cerutti@live.com>
2023-01-23 21:13:21 -0300

    education texts implementation

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import { useTranslation } from "react-i18next";
 import {
     VerticalTimeline,
     VerticalTimelineElement
@@ -7,82 +9,84 @@ import "./styles.scss";
 import SchoolIcon from "@material-ui/icons/School";
 
 function Education() {
+    const { t } = useTranslation();
+
     return (
         <section className="section education" id="education">
             <div>
-                <h1>Education</h1>
+                <h1>{t("education")}</h1>
                 <VerticalTimeline>
                     <VerticalTimelineElement
                         contentStyle={{ background: "#45A29E", color: "#8892b0" }}
 
-                        date="2.5 years"
+                        date={t("education_date_1")}
                         dateClassName="education-date"
                         iconStyle={{ background: "#66FCF1", color: "#1F2833" }}
                         icon={<SchoolIcon />}
                     >
-                        <h3>Analysis and systems development</h3>
+                        <h3>{t("education_course_header_1")}</h3>
                         <a href="https://www.unicesumar.edu.br/ead/cursos-graduacao/analise-e-desenvolvimento-de-sistemas/"></a>
-                        <h5>College degree - 2024.</h5>
+                        <h5>{t("education_course_text_1")}</h5>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         contentStyle={{ background: "#45A29E", color: "#8892b0" }}
 
-                        date="100 hours"
+                        date={t("education_date_2")}
                         dateClassName="education-date"
                         iconStyle={{ background: "#66FCF1", color: "#1F2833" }}
                         icon={<SchoolIcon />}
                     >
-                        <h3>Dev em Dobro</h3>
+                        <h3>{t("education_course_header_2")}</h3>
                         <a href="https://dev-em-dobro.members.ticto.com.br/portal"></a>
-                        <h5>Complete course for front-end web development, including: HTML5, CSS3, JavaScript (ES6) and ReactJS.</h5>
+                        <h5>{t("education_course_text_2")}</h5>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         contentStyle={{ background: "#45A29E", color: "#8892b0" }}
 
-                        date="14 hours"
+                        date={t("education_date_3")}
                         dateClassName="education-date"
                         iconStyle={{ background: "#66FCF1", color: "#1F2833" }}
                         icon={<SchoolIcon />}
                     >
-                        <h3>TypeScript - From basic to advanced</h3>
+                        <h3>{t("education_course_header_3")}</h3>
                         <a href="https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/"></a>
-                        <h5>Complete course of TypeScript, ReactJS and Express.</h5>
+                        <h5>{t("education_course_text_3")}</h5>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         contentStyle={{ background: "#45A29E", color: "#8892b0" }}
 
-                        date="24 hours"
+                        date={t("education_date_4")}
                         dateClassName="education-date"
                         iconStyle={{ background: "#66FCF1", color: "#1F2833" }}
                         icon={<SchoolIcon />}
                     >
-                        <h3>Full project with NodeJS, React, React Native and TypeScript</h3>
+                        <h3>{t("education_course_header_4")}</h3>
                         <a href="https://www.udemy.com/course/dev-fullstack/"></a>
-                        <h5>Step-by-step development of a fullstack project.</h5>
+                        <h5>{t("education_course_text_4")}</h5>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         contentStyle={{ background: "#45A29E", color: "#8892b0" }}
 
-                        date="5 years"
+                        date={t("education_date_5")}
                         dateClassName="education-date"
                         iconStyle={{ background: "#66FCF1", color: "#1F2833" }}
                         icon={<SchoolIcon />}
                     >
-                        <h3>English language</h3>
+                        <h3>{t("education_course_header_5")}</h3>
                         <a href="https://fisk.com.br/"></a>
-                        <h5>Complete english course at Fisk language school.</h5>
+                        <h5>{t("education_course_text_5")}</h5>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         contentStyle={{ background: "#45A29E", color: "#8892b0" }}
 
-                        date="4 years"
+                        date={t("education_date_6")}
                         dateClassName="education-date"
                         iconStyle={{ background: "#66FCF1", color: "#1F2833" }}
                         icon={<SchoolIcon />}
                     >
-                        <h3>Spanish language</h3>
+                        <h3>{t("education_course_header_6")}</h3>
                         <a href="https://fisk.com.br/"></a>
-                        <h5>Complete spanish course at Fisk language school.</h5>
+                        <h5>{t("education_course_text_6")}</h5>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
             </div>
